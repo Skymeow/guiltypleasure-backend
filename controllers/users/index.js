3a40006f0ba341811f5d5343348250fbb3febb70
Sky Xu <skyrocks88xmz@gmail.com>
2017-04-05 09:16:35 -0400

    clearnup

--- controllers/users/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('./controller');
-
-router.get('/dashboard', controller.authorizeToken);
-router.post('/login', controller.login);
-router.post('/signup', controller.create);
-
-module.exports = router;
